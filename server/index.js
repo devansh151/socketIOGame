e863d488b11a097b8d4c86ff52707a9729e4c7a9
Devansh Khanwalkar <devansh.khanwalkar@walmartlabs.com>
2020-02-22 16:08:53 +0530

    port config

--- server/index.js
+++ server/index.js
@@ -117,5 +117,6 @@ io.sockets.on('connection', (socket) => {
 })
 
 // starting server
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
 console.log('server is running on port 3000 ....')
